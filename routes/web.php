7076bbb8e45c9622bad710eb05087b7c279e877f
NinjaLee <tanin.boss4@gmail.com>
2023-03-27 13:45:46 +0700

    test admin and user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,6 @@ Route::get('/test-connection', function () {
 Route::get('/leave_system', function () {
     return view('leave_system');
 });
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin/home',[HomeController::class,'adminHome'])->name('admin.home')->middleware('is_admin');
