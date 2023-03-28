adc703d0586f66de07c55e4b78e2f4ba0744c7fe
NinjaLee <tanin.boss4@gmail.com>
2023-03-28 09:41:22 +0700

    login user page and admin page

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -1,25 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<html>
+    <head>
+        <title>New Page</title>
+    </head>
+    <body>
+        <h1>Welcome to the Leave System!</h1>
+        <p>You are Admin.</p>
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "leave_system";
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-
-                    You are Admin.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            $query = "SELECT `leave`.leave_id, `leave`.starts_date, `leave`.end_date, leavetype.leave_type_name, employee.first_name, employee.last_name,
+            document.document_id, document.comment, document.image
+            FROM `leave`
+            JOIN leavetype ON `leave`.leave_type_id = leavetype.leave_type_id
+            JOIN employee ON `leave`.employee_id = employee.employee_id
+            JOIN document ON `leave`.document_id = document.document_id 
+            ORDER BY `leave`.leave_id DESC";
+            $result = $conn->query($query);
+            if (!$result) {
+                die("Query failed: " . $conn->error);
+            }
+            while ($row = $result->fetch_assoc()) {
+                echo "Leave ID: " . $row['leave_id'] . "<br>";
+                echo "Start Date: " . $row['starts_date'] . "<br>";
+                echo "End Date: " . $row['end_date'] . "<br>";
+                echo "Leave Type: " . $row['leave_type_name'] . "<br>";
+                echo "Employee Name: " . $row['first_name'] . " " . $row['last_name'] . "<br>";
+                echo "Document ID: " . $row['document_id'] . "<br>";
+                echo "Comment: " . $row['comment'] . "<br>";
+                echo "เอกสารที่เกี่ยวข้อง"."<br>";
+                $imgData = base64_encode($row['image']);
+                echo '<img src="data:image/jpeg;base64,'.$imgData.'">'."<br>";
+            }
+            $conn->close();
+        ?>
+    </body>
+</html>
 @endsection
